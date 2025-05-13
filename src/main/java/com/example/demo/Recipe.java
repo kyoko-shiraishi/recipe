2f0accdfc4d598437b90b88f9da7860f273e3fdb
kyon0704 <kyon0704@docomo.ne.jp>
2025-05-13 19:27:40 +0900

    method

--- src/main/java/com/example/demo/Recipe.java
+++ src/main/java/com/example/demo/Recipe.java
@@ -18,17 +18,13 @@ public class Recipe {
 	@Column(length=50,nullable=false)
 	private String name; //レシピと料理名は一対一なのでリレーションなし
 	
-	@Column
-	private String tag; //レシピとタグは一対多なのでtagsテーブルにrecipe_id
+	
 	
 	@Column
-	private String ingridient; //レシピと材料は一対多なのでingridientsテーブルにrecipe_id
+	private String comment; //レシピとコメントの記述は一対多なのでdescriptionsテーブルにrecipe_id
 	
 	@Column
-	private long comment; //レシピとコメントの記述は一対多なのでdescriptionsテーブルにrecipe_id
-	
-	@Column
-	private int cookin_time; //レシピと料理時間は一対一なのでリレーションなし
+	private int cooking_time; //レシピと料理時間は一対一なのでリレーションなし
 	
 	@Column
 	private int servings; //レシピと人数は一対一なのでリレーションなし
@@ -38,4 +34,47 @@ public class Recipe {
 	
 	@Column
 	private String main_image; //レシピとメイン画像は一対一なのでリレーションなし
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	public int getCookingTime() {
+		return cooking_time;
+	}
+	public void setCookingTime(int cooking_time) {
+		this.cooking_time = cooking_time;
+	}
+	public int getServings() {
+		return servings;
+	}
+	public void setServings(int servings) {
+		this.servings = servings;
+	}
+	public int getUser() {
+		return user_id;
+	}
+	public void setUser(int user_id) {
+		this.user_id = user_id;
+	}
+	public String getMainImg() {
+		return main_image;
+	}
+	public void setMainImg(String MainImg) {
+		this.main_image = MainImg;
+	}
 }
