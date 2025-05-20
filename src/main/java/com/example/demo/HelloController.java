e5dff4d81a9b6d6ad18e930bb096118b5561ea50
kyon0704 <kyon0704@docomo.ne.jp>
2025-05-20 15:56:17 +0900

    Move CookingRepository to demo.repository and fix controller

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,24 @@
 package com.example.demo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.repository.CookingRepository;
+
 
 @Controller
 public class HelloController {
+	@Autowired
+	CookingRepository repository;
 	@RequestMapping("/")
-	public String index() {
-		return "index";
+	public ModelAndView index(ModelAndView mav) {
+		mav.setViewName("index");
+		mav.addObject("msg","ようこそ！");
+		return mav;
+		
 	}
-	//indx.htmlを表示するメソッド
+	
 
 }
 
