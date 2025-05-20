f0b0d09d3959dffb6fd9e66aa79735198d8b65da
kyon0704 <kyon0704@docomo.ne.jp>
2025-05-20 17:12:15 +0900

    formModel

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,6 +1,9 @@
 package com.example.demo;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -11,15 +14,18 @@ import com.example.demo.repository.CookingRepository;
 public class HelloController {
 	@Autowired
 	CookingRepository repository;
+	// "/"にアクセスしたらmsgとコントロール、レコード一覧を表示
 	@RequestMapping("/")
-	public ModelAndView index(ModelAndView mav) {
+	public ModelAndView index(@ModelAttribute("formModel") Recipe recipe,ModelAndView mav) {
 		mav.setViewName("index");
 		mav.addObject("msg","ようこそ！");
+		List<Recipe> list = repository.findAll();
+		mav.addObject("data",list);
 		return mav;
 		
 	}
 	
-
+	
 }
 
 
