5151bc3d9c2b38ad4f420a9c0e8d275a082fcabd
kyon0704 <kyon0704@docomo.ne.jp>
2025-06-03 16:56:10 +0900

    override1

--- src/main/java/com/example/demo/Logic/Ninja.java
+++ src/main/java/com/example/demo/Logic/Ninja.java
@@ -2,8 +2,8 @@ package com.example.demo.Logic;
 
 public class Ninja extends Character{
 	//コンストラクタ
-	public Ninja() {
-		super("尊奈門"); //親クラスである Character のコンストラクタを呼び出している。new Ninjaされるときに呼ばれる
+	public Ninja(String name) {
+		super(name); //親クラスである Character のコンストラクタを呼び出している。new Ninjaされるときに呼ばれる
 	}
 	
 	@Override
@@ -20,10 +20,22 @@ public class Ninja extends Character{
 	public String getEnd() {
 		return "ぞ";
 	}
+	@Override
+	public boolean equals(Object o) {
+		if(o == this)return true; //もし2つのオブジェクトが**まったく同じ実体（参照）**なら、中身が同じに決まっているので true を返す
+		//自分のクラス情報と相手のクラス情報が一致しなければ、別のクラスのインスタンスなので false
+		if(o == null||getClass() != o.getClass())return false;
+		//Object 型なので、Ninja 型にキャストして使えるようにする
+		Ninja other  = (Ninja) o;
+		//「自分と相手の name が同じ文字列なら、等しいキャラとみなす
+		return this.name.equals(other.name); //これ自体戻り値が boolean 値なのでそのままreturnできる
+	}
+	
 		public static void main(String[] args) {
-        Character c = new Ninja();
+        Character c = new Ninja("そんなもん");
+        Character fake = new Ninja("くみがしら");
         c.act();  // 動作確認
-        System.out.println(c);
+        System.out.println(c.equals(fake));
         AlermTimer timer = new AlermTimer();
         int timePassed = 1;
         timer.startTimer(timePassed, c);
