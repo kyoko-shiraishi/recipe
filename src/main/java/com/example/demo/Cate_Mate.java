e31fc6cbcbaac0bb952bd199f76901ec8a1fc3c2
kyon0704 <kyon0704@docomo.ne.jp>
2025-07-01 18:43:11 +0900

    middle_table

---
+++ src/main/java/com/example/demo/Cate_Mate.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import jakarta.persistence.Column; 
+import java.util.List;
+import java.util.ArrayList;
+import com.example.demo.*;
+import jakarta.persistence.Entity; 
+import jakarta.persistence.GeneratedValue; 
+import jakarta.persistence.GenerationType; 
+import jakarta.persistence.Id; 
+import jakarta.persistence.Table; 
+import jakarta.persistence.ManyToOne; //追記
+import jakarta.persistence.JoinColumn;
+@Entity
+public class Cate_Mate {
+
+}
